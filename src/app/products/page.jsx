6747c182f24e37a075b73e341660e2a88a697e17
Likhon-25 <likhonmondol327@gmail.com>
2026-09-19 01:02:37 +0600

    create data for json-server and 5000 port build

---
+++ src/app/products/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const getProducts = async () =>{
+
+}
+const ProductsPage = () => {
+    return (
+        <div>
+            <h2>Products</h2>
+        </div>
+    );
+};
+
+export default ProductsPage;
